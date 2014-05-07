68a3997674fc6fdd97aae9d10c39aa06832340db
Anthony Ringoet <anthonyringoet@gmail.com>
2014-05-07 19:00:57 +0200

    bring in hbs

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,22 @@
-var source = require('vinyl-source-stream')
-var streamify = require('gulp-streamify')
-var browserify = require('browserify')
-var uglify = require('gulp-uglify')
-var gulpify = require('gulpify')
-var gulp = require('gulp')
+var source = require('vinyl-source-stream');
+var streamify = require('gulp-streamify');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var gulpify = require('gulpify');
+var gulp = require('gulp');
 
 
-// var hbsfy = require("hbsfy").configure({
-//   extensions: ["html"]
-// });
+var hbsfy = require('hbsfy').configure({
+  extensions: ['html', 'hbs']
+});
 
 gulp.task('browserify', function() {
-  var bundleStream = browserify('./js/app.js').bundle()
+  var bundleStream = browserify('./js/app.js')
+    .transform(hbsfy)
+    .bundle();
 
   bundleStream
     .pipe(source('./bundle.js'))
     .pipe(streamify(uglify()))
-    .pipe(gulp.dest('./js/build/'))
+    .pipe(gulp.dest('./js/build/'));
 })
--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-// var template = require("./templates/foo.hbs");
-// document.body.innerHTML = template({ name: "Foobar" });
+var template = require("./templates/foo.hbs");
+document.body.innerHTML = template({ name: "Foobar" });
 
 var url = require('url');
 console.log('foo');
